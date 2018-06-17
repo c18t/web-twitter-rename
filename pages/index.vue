1628a9d1c33209122dc8a0c696a786b5c789c41b
鵜巷 <u@chimata.org>
2018-06-18 04:33:26 +0900

    add twitter share button
    - 変更時のポップアップ多分ブロックされるのでシェアボタンを設置

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
       <div class="rename-form-wapper">(/ɯ̹t͡ɕʲi/) <RenameForm /></div>
       <p>とします！</p>
     </div>
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="@c18t の名前を変更してあげました！すごい！" data-show-count="false">Tweet</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   </section>
 </template>
 
